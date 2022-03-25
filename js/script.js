8c75a1e93972eeec31c269f8c91328f50853543b
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-25 14:42:32 +0100

    add first step complete

--- js/script.js
+++ js/script.js
@@ -28,6 +28,39 @@ const slides = [
 
 console.log(slides);
 
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
 const app = new Vue({
   el: "#app",
+  data: {
+    slides,
+    activeSlideIndex: 0,
+  },
+  methods: {
+    // se mi trovo ad un index > 0 allora posso andare indietro
+    showPrevSlide() {
+      if (this.activeSlideIndex > 0) {
+        this.activeSlideIndex--;
+      } else {
+        this.activeSlideIndex = this.slides.length - 1;
+      }
+    },
+    // se mi trovo ad un index < della lunghezza di slides  allora posso andare avanti
+    showNextSlide() {
+      if (this.activeSlideIndex < this.slides.length - 1) {
+        this.activeSlideIndex++;
+      } else {
+        this.activeSlideIndex = 0;
+      }
+    },
+
+    showIfActive(item) {
+      // vado a prendere il titolo che è ugualeal titolo dell'index di slides
+      const isActive = item.title === this.slides[this.activeSlideIndex].title;
+      // se true applico thumb active, se false thumb
+      return isActive ? "thumb active" : "thumb";
+    },
+  },
 });
